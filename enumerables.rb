a61c11c7298219a406a52b0974f8e41505e41dd0
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-12 16:09:10 +0300

    create my_each_with_index, add an extra index on the my_each

--- enumerables.rb
+++ enumerables.rb
@@ -3,8 +3,17 @@ module Enumerable
       i = 0
       while i < self.size
         yield(self[i])
-        i+=1
+        i += 1
       end
+    end
+
+    def my_each_with_index
+        i = 0
+        while i < self.size
+            yield(self[i], i)
+            i =+ 1
+        end
+        self
     end
 
 
