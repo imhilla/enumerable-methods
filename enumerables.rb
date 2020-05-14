039990b356e0cd703b206cdc5ced1801ff618852
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-14 20:01:47 +0300

    Update enumerables.rb

--- enumerables.rb
+++ enumerables.rb
@@ -98,20 +98,21 @@ end
   puts text
 end
 
-hash = Hash.new
-%w(cat dog wombat).my_each_with_index { |item, index|
-  hash[item] = index
-}
-puts hash 
+[5, 10, 15, 20, 25, 30].my_each_with_index do |num, idx| 
+  puts "#{num}"
+  if ((idx) % 2 == 0) 
+    puts "end of line" 
+  end 
+end 
 
-a = [18, 22, 33, 3, 5, 6] 
-puts "my_select method : #{ a.my_select { |num| num > 10 } }\n\n"
+a = [18, 22, 33, 3, 5, 6]
+puts "my_select method : #{a.my_select { |num| num > 10 } }\n\n"
 
 enu1 = [10, 19, 18]
-res1 = enu1.my_all? { |num| num >4 }
+res1 = enu1.my_all? { |num| num > 4 }
 puts res1
 
-enu1 = [10, 19, 18] 
+enu1 = [10, 19, 18]
 res1 = enu1.none? { |num| num > 4 }
 puts res1
 
@@ -123,7 +124,7 @@ a = [18, 22, 33, nil, 5, 6]
 puts "counting : #{a.my_count}\n\n"
 
 a = [18, 22, 33, 3, 5, 6]
-puts "map method : #{ a.my_map { |num| num > 10 } }\n\n"
+puts "map method : #{a.my_map { |num| num > 10 } }\n\n"
 
 def sum(array)
   array.my_inject(0) { |sum, num| sum + num }
