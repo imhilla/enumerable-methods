1897143b3ebc432d5f12ae837edb5a6766326018
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-14 20:09:01 +0300

    Update enumerables.rb

--- enumerables.rb
+++ enumerables.rb
@@ -99,7 +99,7 @@ end
 end
 
 a = [18, 22, 33, 3, 5, 6]
-puts "my_select method :#{a.my_select { |num| num > 10 } }\n\n"
+puts "my_select method :#{a.my_select { |num| num > 10 }}\n\n"
 
 enu1 = [10, 19, 18]
 res1 = enu1.my_all? { |num| num > 4 }
@@ -117,7 +117,7 @@ a = [18, 22, 33, nil, 5, 6]
 puts "counting : #{a.my_count}\n\n"
 
 a = [18, 22, 33, 3, 5, 6]
-puts "map method :#{a.my_map { |num| num > 10 } }\n\n"
+puts "map method :#{a.my_map { |num| num > 10 }}\n\n"
 
 def sum(array)
   array.my_inject(0) { |sum, num| sum + num }
