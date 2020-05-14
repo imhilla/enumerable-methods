108f5fa27f075fc3dce996f124a233cfa22c4798
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-14 19:48:03 +0300

    Update enumerables.rb

--- enumerables.rb
+++ enumerables.rb
@@ -91,3 +91,41 @@ module Enumerable
     accumulator
   end
 end
+
+# tests
+[1, 2, 3].my_each do |n|
+  text = "Current number is: #{n}"
+  puts text
+end
+
+hash = Hash.new
+%w(cat dog wombat).my_each_with_index { |item, index|
+  hash[item] = index
+}
+puts hash 
+
+a = [18, 22, 33, 3, 5, 6] 
+puts "my_select method : #{a.my_select {|num| num > 10 }}\n\n"
+
+enu1 = [10, 19, 18] 
+res1 = enu1.my_all? { |num| num>4}
+puts res1
+
+enu1 = [10, 19, 18] 
+res1 = enu1.none? { |num| num>4}
+puts res1
+
+enu1 = [10, 19, 18] 
+res1 = enu1.any? { |num| num>4}
+puts res1
+
+a = [18, 22, 33, nil, 5, 6]
+puts "counting : #{a.my_count}\n\n"
+
+a = [18, 22, 33, 3, 5, 6] 
+puts "map method : #{a.my_map {|num| num > 10 }}\n\n"
+
+def sum(array)
+  array.my_inject(0) { |sum, num| sum + num }
+end
+p sum([5, 10, 20])
